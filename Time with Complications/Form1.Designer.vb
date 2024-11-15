b0d3479fa7dc3af6bca05252a9394abd99a7637c
Joseph Lumbley <joelumbley16@gmail.com>
2024-11-14 21:08:59 -0600

    Add project files.

---
+++ Time with Complications/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Timer1 = New Timer(components)
+        MenuStrip1 = New MenuStrip()
+        MainToolStripMenuItem = New ToolStripMenuItem()
+        TwelveHourMenuItem = New ToolStripMenuItem()
+        TwentyFourHourMenuItem = New ToolStripMenuItem()
+        TopToolStripMenuItem = New ToolStripMenuItem()
+        TopDayMenuItem = New ToolStripMenuItem()
+        TopShortDayMenuItem = New ToolStripMenuItem()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.ImageScalingSize = New Size(24, 24)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {MainToolStripMenuItem, TopToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 33)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' MainToolStripMenuItem
+        ' 
+        MainToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {TwelveHourMenuItem, TwentyFourHourMenuItem})
+        MainToolStripMenuItem.Name = "MainToolStripMenuItem"
+        MainToolStripMenuItem.Size = New Size(67, 29)
+        MainToolStripMenuItem.Text = "Main"
+        ' 
+        ' TwelveHourMenuItem
+        ' 
+        TwelveHourMenuItem.Checked = True
+        TwelveHourMenuItem.CheckState = CheckState.Checked
+        TwelveHourMenuItem.Name = "TwelveHourMenuItem"
+        TwelveHourMenuItem.Size = New Size(179, 34)
+        TwelveHourMenuItem.Text = "12 Hour"
+        ' 
+        ' TwentyFourHourMenuItem
+        ' 
+        TwentyFourHourMenuItem.Name = "TwentyFourHourMenuItem"
+        TwentyFourHourMenuItem.Size = New Size(179, 34)
+        TwentyFourHourMenuItem.Text = "24 Hour"
+        ' 
+        ' TopToolStripMenuItem
+        ' 
+        TopToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {TopDayMenuItem, TopShortDayMenuItem})
+        TopToolStripMenuItem.Name = "TopToolStripMenuItem"
+        TopToolStripMenuItem.Size = New Size(57, 29)
+        TopToolStripMenuItem.Text = "Top"
+        ' 
+        ' TopDayMenuItem
+        ' 
+        TopDayMenuItem.Checked = True
+        TopDayMenuItem.CheckState = CheckState.Checked
+        TopDayMenuItem.Name = "TopDayMenuItem"
+        TopDayMenuItem.Size = New Size(270, 34)
+        TopDayMenuItem.Text = "Day"
+        ' 
+        ' TopShortDayMenuItem
+        ' 
+        TopShortDayMenuItem.Name = "TopShortDayMenuItem"
+        TopShortDayMenuItem.Size = New Size(270, 34)
+        TopShortDayMenuItem.Text = "Short Day"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MainToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TwelveHourMenuItem As ToolStripMenuItem
+    Friend WithEvents TwentyFourHourMenuItem As ToolStripMenuItem
+    Friend WithEvents TopToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TopDayMenuItem As ToolStripMenuItem
+    Friend WithEvents TopShortDayMenuItem As ToolStripMenuItem
+
+End Class
---
+++ Time with Complications/Form1.vb
@@ -0,0 +1,355 @@
+﻿' Time🕒 with Complications
+
+' MIT License
+' Copyright(c) 2024 Joseph W. Lumbley
+
+' Permission Is hereby granted, free Of charge, to any person obtaining a copy
+' of this software And associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, And/Or sell
+' copies of the Software, And to permit persons to whom the Software Is
+' furnished to do so, subject to the following conditions:
+
+' The above copyright notice And this permission notice shall be included In all
+' copies Or substantial portions of the Software.
+
+' THE SOFTWARE Is PROVIDED "AS IS", WITHOUT WARRANTY Of ANY KIND, EXPRESS Or
+' IMPLIED, INCLUDING BUT Not LIMITED To THE WARRANTIES Of MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE And NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS Or COPYRIGHT HOLDERS BE LIABLE For ANY CLAIM, DAMAGES Or OTHER
+' LIABILITY, WHETHER In AN ACTION Of CONTRACT, TORT Or OTHERWISE, ARISING FROM,
+' OUT OF Or IN CONNECTION WITH THE SOFTWARE Or THE USE Or OTHER DEALINGS IN THE
+' SOFTWARE.
+
+' Monica is our an AI assistant.
+' https://monica.im/
+
+Public Class Form1
+
+    Private Context As BufferedGraphicsContext
+    Private Buffer As BufferedGraphics
+    'Private DisplayText As String
+    'Private TimeFont As New Font("Segoe UI", 12, FontStyle.Regular)
+    'Private TimeFontSize As Single
+    'Private TimePosition As New Point(100, 0)
+    Private TimeZone As String
+
+    'Private Zone As String
+
+    Private TimeZoneCity As String
+
+    Private Enum HourFormat
+        Twelve
+        TwentyFour
+    End Enum
+
+    Private Enum InfoType
+        Time
+        LongDayOfWeek
+        ShortDayOfWeek
+        LongDate
+        MedDate
+        ShortDate
+        TimeZone
+        TimeZoneCity
+    End Enum
+
+    Private Hours As HourFormat = HourFormat.Twelve
+
+    Private Structure DisplayObject
+        Public location As Point
+        Public text As String
+        Public font As Font
+        Public info As InfoType
+
+    End Structure
+
+    Private MainDisplay As DisplayObject
+    Private TopDisplay As DisplayObject
+    Private BottomDisplay As DisplayObject
+
+
+
+    Private ReadOnly AlineCenterMiddle As New StringFormat With {.Alignment = StringAlignment.Center, .LineAlignment = StringAlignment.Center}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        InitializeForm()
+
+        InitializeBuffer()
+
+        TopDisplay.info = InfoType.LongDayOfWeek
+
+        BottomDisplay.info = InfoType.MedDate
+
+        Timer1.Interval = 20
+
+        Timer1.Enabled = True
+
+        Debug.Print($"Program running... {Now.ToShortTimeString}")
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If Hours = HourFormat.Twelve Then
+
+            ' Formats the current time to 12-hour (Regular Time)
+            MainDisplay.text = Now.ToLocalTime.ToShortTimeString()
+
+        Else
+
+            ' Formats the current time to 24-hour (Military Time)
+            MainDisplay.text = Now.ToLocalTime.ToString("HH:mm")
+
+        End If
+
+        Select Case TopDisplay.info
+
+            Case InfoType.LongDayOfWeek
+
+                TopDisplay.text = Now.DayOfWeek.ToString
+
+            Case InfoType.ShortDayOfWeek
+
+                Select Case Now.DayOfWeek
+                    Case DayOfWeek.Sunday
+                        TopDisplay.text = "Sun"
+                    Case DayOfWeek.Monday
+                        TopDisplay.text = "Mon"
+                    Case DayOfWeek.Tuesday
+                        TopDisplay.text = "Tue"
+                    Case DayOfWeek.Wednesday
+                        TopDisplay.text = "Wed"
+                    Case DayOfWeek.Thursday
+                        TopDisplay.text = "Thu"
+                    Case DayOfWeek.Friday
+                        TopDisplay.text = "Fri"
+                    Case DayOfWeek.Saturday
+                        TopDisplay.text = "Sat"
+                End Select
+
+        End Select
+
+
+
+
+
+        'Dim timeZone As TimeZoneInfo = TimeZoneInfo.Local
+
+        'If timeZone = TimeZoneInfo.Id Then
+
+
+        'End If
+
+        Select Case TimeZoneInfo.Local.Id
+            Case "Eastern Standard Time"
+                TimeZoneCity = "New York"
+            Case "Central Standard Time"
+                TimeZoneCity = "Chicago"
+            Case "Mountain Standard Time"
+                TimeZoneCity = "Denver"
+            Case "Pacific Standard Time"
+                TimeZoneCity = "Los Angeles"
+            Case Else
+                TimeZoneCity = "Unknown"
+        End Select
+
+
+
+
+        'TopDisplay.text = Now.DayOfWeek.ToString
+        'BottomDisplay.text = Now.Date()
+        BottomDisplay.text = Now.ToString("MMMM d, yyyy")
+
+
+        'TopDisplay.text = Now.ToLocalTime.ToShortTimeString() & Environment.NewLine & TimeZoneCity ' Formats the current time to 12-hour format and time zone
+
+
+        'DisplayText = Now.ToLocalTime.ToString("HH:mm") ' Formats the current time to 24-hour format (military time)
+
+        Refresh() ' Calls OnPaint Sub
+
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+
+        ' Set the font size for the main display based on the width of the client rectangle
+        Dim FontSize As Integer = ClientRectangle.Width \ 12
+
+        MainDisplay.font = New Font("Segoe UI", FontSize, FontStyle.Regular)
+
+        ' Center the main display in the client rectangle.
+        MainDisplay.location.X = ClientRectangle.Width \ 2
+        MainDisplay.location.Y = ClientRectangle.Height \ 2
+
+        FontSize = ClientRectangle.Width \ 35
+
+        TopDisplay.font = New Font("Segoe UI", FontSize, FontStyle.Regular)
+
+
+        TopDisplay.location.X = ClientRectangle.Width \ 2
+        TopDisplay.location.Y = ClientRectangle.Height \ 2 - ClientRectangle.Width \ 8
+
+
+        FontSize = ClientRectangle.Width \ 35
+
+        BottomDisplay.font = New Font("Segoe UI", FontSize, FontStyle.Regular)
+
+
+        BottomDisplay.location.X = ClientRectangle.Width \ 2
+        BottomDisplay.location.Y = ClientRectangle.Height \ 2 + ClientRectangle.Width \ 8
+
+
+        ' Dispose of the existing buffer
+        If Buffer IsNot Nothing Then
+
+            Buffer.Dispose()
+
+            Buffer = Nothing ' Set to Nothing to avoid using a disposed object
+
+        End If
+
+        ' The buffer will be reallocated in OnPaint
+
+    End Sub
+
+    Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
+
+        ' Allocate the buffer if it hasn't been allocated yet
+        If Buffer Is Nothing Then
+
+            Buffer = Context.Allocate(e.Graphics, ClientRectangle)
+
+        End If
+
+        DrawFrame()
+
+        Buffer.Render(e.Graphics)
+
+    End Sub
+
+    Private Sub DrawFrame()
+
+        If Buffer IsNot Nothing Then
+
+            Try
+
+                'Using font As New Font("Segoe UI", TimeFontSize, FontStyle.Regular)
+
+                'Dim DisplayText As New String(Now.ToLocalTime.ToShortTimeString() & Environment.NewLine & Now.ToLocalTime.DayOfWeek.ToString())
+                'Dim DisplayText As New String(Now.ToLocalTime.ToShortTimeString())
+
+
+                With Buffer.Graphics
+
+                    '.CompositingMode = Drawing2D.CompositingMode.SourceCopy
+                    '.SmoothingMode = Drawing2D.SmoothingMode.None
+                    '.PixelOffsetMode = Drawing2D.PixelOffsetMode.None
+
+                    .Clear(Color.Black)
+
+                    .CompositingMode = Drawing2D.CompositingMode.SourceOver
+                    .TextRenderingHint = Drawing.Text.TextRenderingHint.AntiAlias
+                    .SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+                    .PixelOffsetMode = Drawing2D.PixelOffsetMode.None
+                    .CompositingQuality = Drawing2D.CompositingQuality.HighQuality
+
+                    .DrawString(MainDisplay.text, MainDisplay.font, Brushes.White, MainDisplay.location, AlineCenterMiddle)
+
+                    .DrawString(TopDisplay.text, TopDisplay.font, Brushes.LightGray, TopDisplay.location, AlineCenterMiddle)
+
+                    .DrawString(BottomDisplay.text, BottomDisplay.font, Brushes.LightGray, BottomDisplay.location, AlineCenterMiddle)
+
+
+                End With
+
+                'End Using
+
+            Catch ex As Exception
+
+                Debug.Print("Draw error: " & ex.Message)
+
+            End Try
+
+        Else
+
+            Debug.Print("Buffer is not initialized.")
+
+        End If
+
+    End Sub
+
+
+    Private Sub InitializeForm()
+
+        CenterToScreen()
+
+        SetStyle(ControlStyles.UserPaint, True)
+
+        SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
+
+        SetStyle(ControlStyles.AllPaintingInWmPaint, True)
+
+        Text = "Time🕒 with Complications - Code with Joe"
+
+        Me.WindowState = FormWindowState.Maximized
+
+    End Sub
+
+    Private Sub InitializeBuffer()
+
+        ' Set context to the context of this app.
+        Context = BufferedGraphicsManager.Current
+
+        Context.MaximumBuffer = Screen.PrimaryScreen.WorkingArea.Size
+
+        ' Allocate the buffer initially using the current client rectangle
+        Buffer = Context.Allocate(CreateGraphics(), ClientRectangle)
+
+    End Sub
+
+    Private Sub TwentyFourHourMenuItem_Click(sender As Object, e As EventArgs) Handles TwentyFourHourMenuItem.Click
+
+        If Not Hours = HourFormat.TwentyFour Then Hours = HourFormat.TwentyFour
+
+        If Not TwentyFourHourMenuItem.Checked Then TwentyFourHourMenuItem.Checked = True
+
+        If TwelveHourMenuItem.Checked Then TwelveHourMenuItem.Checked = False
+
+    End Sub
+
+    Private Sub TwelveHourMenuItem_Click(sender As Object, e As EventArgs) Handles TwelveHourMenuItem.Click
+
+        If Not Hours = HourFormat.Twelve Then Hours = HourFormat.Twelve
+
+        If Not TwelveHourMenuItem.Checked Then TwelveHourMenuItem.Checked = True
+
+        If TwentyFourHourMenuItem.Checked Then TwentyFourHourMenuItem.Checked = False
+
+    End Sub
+
+    Private Sub TopDayMenuItem_Click(sender As Object, e As EventArgs) Handles TopDayMenuItem.Click
+
+        If Not TopDisplay.info = InfoType.LongDayOfWeek Then TopDisplay.info = InfoType.LongDayOfWeek
+
+        If Not TopDayMenuItem.Checked Then TopDayMenuItem.Checked = True
+
+        If TopShortDayMenuItem.Checked Then TopShortDayMenuItem.Checked = False
+
+
+    End Sub
+
+    Private Sub TopShortDayMenuItem_Click(sender As Object, e As EventArgs) Handles TopShortDayMenuItem.Click
+
+        If Not TopDisplay.info = InfoType.ShortDayOfWeek Then TopDisplay.info = InfoType.ShortDayOfWeek
+
+        If Not TopShortDayMenuItem.Checked Then TopShortDayMenuItem.Checked = True
+
+        If TopDayMenuItem.Checked Then TopDayMenuItem.Checked = False
+
+    End Sub
+
+
+
+End Class
